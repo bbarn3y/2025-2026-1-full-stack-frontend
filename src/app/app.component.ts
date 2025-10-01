bc847ea1bf7605668875725872a36d63de846a62
barnabas.balazs <barnabas.balazs@enliven.systems>
2025-10-01 17:52:14 +0200

    2. gyakorlat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent, RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   title = '2025-2026-2-full-stack';
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.less'
+})
+export class LoginComponent {
+
+}
